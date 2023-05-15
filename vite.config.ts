d4c5bbd782ccbfc3cf57719d875ad56788e4a771
Nikolai Muratov <el.muratovnik@gmail.com>
2023-05-15 14:39:09 +0300

    path fixes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/example-ret/",
+  base: "./",
   build: {
     outDir: "./dist",
   },
